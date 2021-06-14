8bb5d03cfb87d394817468f5fc84f7749caf36d8
Daniel <artexsdns@gmail.com>
2021-06-14 15:56:29 +0700

    latest deploy api with python 3

--- project/vuedjango/views.py
+++ project/vuedjango/views.py
@@ -21,22 +21,55 @@ def tutorial_list(request):
         if tutorial_serializer.is_valid():
             tutorial_serializer.save()
             return JsonResponse(tutorial_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.method == 'GET':
+            tutorials = Tutorial.objects.all()
+        
+        title = request.GET.get('title', None)
+        if title is not None:
+            tutorials = tutorials.filter(title__icontains=title)
+        
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
+        # 'safe=False' for objects serialization
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def tutorial_detail(request, pk):
     # find tutorial by pk (id)
     try: 
-        tutorial = Tutorial.objects.get(pk=pk) 
+        tutorial = Tutorial.objects.get(pk=pk)
+        if request.method == 'PUT': 
+            tutorial_serializer = TutorialSerializer(tutorial) 
+            return JsonResponse(tutorial_serializer.data) 
+
+        if request.method == 'GET': 
+            tutorial_serializer = TutorialSerializer(tutorial) 
+            return JsonResponse(tutorial_serializer.data) 
+
+        if request.method == 'PUT': 
+            tutorial_data = JSONParser().parse(request) 
+            tutorial_serializer = TutorialSerializer(tutorial, data=tutorial_data) 
+
+        elif request.method == 'DELETE': 
+            tutorial.delete() 
+            return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
+
+        elif request.method == 'DELETE':
+            count = Tutorial.objects.all().delete()
+            return JsonResponse({'message': '{} Tutorials were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+
+        if tutorial_serializer.is_valid(): 
+            tutorial_serializer.save() 
+            return JsonResponse(tutorial_serializer.data) 
+            return JsonResponse(tutorial_serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+            
     except Tutorial.DoesNotExist: 
         return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
- 
-    # GET / PUT / DELETE tutorial
-    
         
 @api_view(['GET'])
 def tutorial_list_published(request):
     # GET all published tutorials
     if request.method == 'GET': 
-        tutorial_serializer = TutorialSerializer(tutorial) 
-        return JsonResponse(tutorial_serializer.data) 
+        tutorials_serializer = TutorialSerializer(tutorials, many=True)
+        return JsonResponse(tutorials_serializer.data, safe=False)
